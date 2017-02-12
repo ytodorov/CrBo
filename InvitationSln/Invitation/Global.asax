2608ce38770c87a16533942bd63ad4aef24317bd
ytodorov <ytodorov@ytodorov.com>
2017-02-12 23:11:24 +0200

    initial

---
+++ InvitationSln/Invitation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Invitation.MvcApplication" Language="C#" %>
---
+++ InvitationSln/Invitation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Invitation.Startup))]
+namespace Invitation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
